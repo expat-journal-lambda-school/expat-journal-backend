8d1660c848aa4e0fd8433c385a5597ff6d31bde6
joe-alfaro <josephlalfaro@gmail.com>
2019-07-31 19:09:13 -0700

    refactor users model and start users update and delete(commented out in router)

--- users/users-model.js
+++ users/users-model.js
@@ -1,18 +1,29 @@
 const db = require('../data/dbConfig.js');
-const Users = db('users');
 
-const findAll = () => Users.select('id', 'username');
+const findAll = () => db('users').select('id', 'username');
 
-const findOneBy = async filter => await Users.where(filter).first();
+const findOneBy = async filter => await db('users').where(filter).first();
 
-const findBy = filter => Users.where(filter);
+const findBy = filter => db('users').where(filter);
 
-const add = async user => await Users.insert(user, ['id']);
+const add = async user => await db('users').insert(user, ['id']);
 
+const update = async (id, user) => {
+  await findOneBy({id}).update(user);
+  return findOneBy({id})
+};
+
+const remove = async (id) => {
+  const deleted = findOneBy({id})
+  findOneBy({id}).del()
+  return deleted
+};
 
 module.exports = {
   findAll, 
   findBy, 
   findOneBy, 
+  update,
+  remove,
   add
 };
--- users/users-router.js
+++ users/users-router.js
@@ -10,7 +10,7 @@ router.get('/', restricted, (req, res) => {
     .catch(error => res.status(500).json(error));
 });
 
-router.get('/:id', restricted, async (req, res) => {
+router.get('/:id', restricted, (req, res) => {
   Users.findOneBy({id: req.params.id})
     .then(user => {
       Posts.findBy({user_id: user.id})
@@ -19,5 +19,32 @@ router.get('/:id', restricted, async (req, res) => {
     })
     .catch(error => res.status(500).json({...error, message: "Error finding user"}))
 })
-
+  /*
+router.put('/:id', restricted, (req, res) => {
+  console.log('from url', Number(req.params.id))
+  console.log('from token', req.decodedToken.subject)
+  if(req.decodedToken.subject === Number(req.params.id)){
+    console.log('received', req.body)
+    Users.update(req.params.id, req.body)
+      .then(updatedUser => res.status(200).json(updatedUser))
+      .catch(error => res.status(500).json(error))
+    }
+  else{
+   res.status(500).json({errorMessage: 'This is not you'})
+  }
+})
+  */
+  /*
+router.delete('/:id', restricted, (req, res) => {
+  if(req.decodedToken.subject === req.params.id){
+    const updates = {username: req.body.username, password: req.body.password};
+    Users.remove(req.params.id, updates)
+      .then(deletedUser => res.status(200).json(updatedUser))
+      .catch(error => res.status(500).json(error))
+    }
+  else{
+   res.status(500).json({errorMessage: 'This is not you'})
+  }
+})
+*/
 module.exports = router;
